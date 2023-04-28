c78c85fd0f0ca883344bc965ad37955215d6d230
genesis <camillemondon@free.fr>
2023-04-28 23:52:27 +0200

    config.js: read paths from env

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 /*
 | Settings for olgitbridge for you to change.
 */
+const path = require('node:path'); 
+
 const config = module.exports = { forward: { }, ssl: { } };
 
 // overleaf server
-config.olServer = 'http://localhost';
+config.olServer = process.env.OVERLEAF_SITE_URL || 'http://localhost';
 
 // used if all the working directories in one place
 // must be ending with '/'
-config.baseDir = '/var/olgitbridge/';
+config.baseDir = path.join(`${process.env.DATA_DIR}`, 'gitbridge/') || '/var/lib/overleaf/gitbridge/';
 
 // blueprint of the overleaf version (only differences to git to be uploaded again)
 config.bluesDir = config.baseDir + 'blues/';
@@ -32,7 +34,7 @@ config.ssl.key = 'XXX';
 config.ssl.cert = 'XXX';
 
 // change to 443 for https
-config.port = 5000;
+config.port = process.env.OVERLEAF_GITBRIDGE_PORT || 5000;
 
 // enable to forward requests to the main port.
 config.forward.enable = false;
